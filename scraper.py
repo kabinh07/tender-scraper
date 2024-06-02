19a744615b5b475fd70caee726a2d55d881208a8
kabinh07 <kanchon@polygontech.xyz>
2024-06-02 12:47:25 +0600

    telebot added

--- scraper.py
+++ scraper.py
@@ -1,22 +1,37 @@
 from selenium import webdriver
 from selenium.webdriver import ChromeOptions
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
-import pandas as pd
 from datetime import datetime, timedelta
+import os
+import telebot
+from telebot.apihelper import ApiTelegramException
 
+# Initializing telegram bot
+# BOT_TOKEN = os.environ.get("BOT_TOKEN")
+BOT_TOKEN = "7201255523:AAEhjZjp0g0YtBmVH1qv0GstvXQg4WbaVK4"
+bot = telebot.TeleBot(BOT_TOKEN)
 
+# Options for chrome driver
 opts = ChromeOptions()
 opts.add_argument("--headless=new")
 
+# URL of the website to scrape
 url = "https://www.eprocure.gov.bd/resources/common/StdTenderSearch.jsp?h=t"
 
+# Webdriver assignment and implicit time delay assign
 driver = webdriver.Chrome()
 driver.implicitly_wait(5)
 
+# Current time assignment
 c_time = datetime.now()
+
+# Set the duration of tenders
 c_time_delayed = c_time - timedelta(hours=2)
 
+# For getting the columns of the table
 def get_tenders_columns():
     table = driver.find_element(By.ID, "resultTable")
     body = table.find_element(By.TAG_NAME, "tbody")
@@ -27,11 +42,13 @@ def get_tenders_columns():
         columns.append(heads.text)
     return columns
 
+# Converting the time into datetime format
 def get_timestamp(time_str):
     time_format = "%d-%b-%Y %H:%M"
     t_time = datetime.strptime(time_str, time_format)
     return t_time
 
+# For getting the table data
 def get_tenders_data():
     row_data = []
     table = driver.find_element(By.ID, "resultTable")
@@ -48,9 +65,17 @@ def get_tenders_data():
             row_data.append(values)
     return row_data
 
+# For getting the tender data in list of json format
 def get_tenders():
     driver.get(url)
     time.sleep(2)
+    try:
+        WebDriverWait(driver, 60).until(
+            EC.presence_of_all_elements_located(By.ID, "resultTable")
+        )
+        print(f"The page fully loaded...")
+    except:
+        print(f"Failed to load the full page...")
     columns = get_tenders_columns()
     data = []
     counter = 1
@@ -66,17 +91,34 @@ def get_tenders():
             counter+=1
             time.sleep(5)
         except:
-            print("No Button")
+            print("No More Next Button...Exiting...!")
             break
-    tenders = []
-    for d in data:
-        tender = {}
-        for i in range(len(columns)):
-            tender[columns[i]] = d[i]
-        tenders.append(tender)
     driver.quit()
-    return tenders
+    return columns, data
+
+def get_group_chat_id():
+    updates = bot.get_updates()
+    for update in updates:
+        if update.message and update.message.chat.type in ['group', 'supergroup']:
+            return update.message.chat.id
 
 if __name__ == "__main__":
-    tenders = get_tenders()
-    print(tenders)
+    group_id = get_group_chat_id()
+    columns, tenders = get_tenders()
+    if len(tenders) > 0:
+        for tender in tenders:
+            print(tender)
+            msg = f"{str(columns[0])}: {str(tender[0])}\n{str(columns[1])}: {str(tender[1])}"
+            while True:
+                try:
+                    bot.send_message(
+                        chat_id = group_id,
+                        text = msg,
+                        allow_sending_without_reply = True
+                        )
+                    time.sleep(2)
+                    break
+                except ApiTelegramException as e:
+                    retry_after = int(e.result_json['parameters']['retry_after'])
+                    print(f"Retrying After {retry_after} seconds...")
+                    time.sleep(retry_after)
