c9658c627570a67c60abe26a53c7fe4337045103
kabinh07 <kanchon@polygontech.xyz>
2024-06-06 19:06:22 +0600

    js script executed

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ opts.add_argument(f'user-agent={random.choice(user_agents)}')
 opts.add_argument("--no-sandbox")
 opts.add_argument("--disable-dev-shm-usage")
 opts.add_argument("--window-size=1920,1080")
-opts.page_load_strategy = 'eager'
+# opts.page_load_strategy = 'eager'
 
 # --------------------- Tender Scraper Start -----------------------------------#
 
@@ -63,6 +63,11 @@ driver_ji.implicitly_wait(5)
 
 driver_jb = webdriver.Chrome(options=opts)
 driver_jb.implicitly_wait(5)
+
+def data_saver(data, out_file):
+    with open(out_file, "a") as f:
+        f.write(data)
+        f.write("\n")
 
 # For getting the columns of the table
 def get_tenders_columns():
@@ -100,6 +105,7 @@ def get_tenders_data():
                     row_data.append(values)
             break
         except StaleElementReferenceException as e:
+            print(f"Exception in Telebot: {e}")
             time.sleep(2)
             table = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "resultTable")))
     return row_data
@@ -115,7 +121,7 @@ def get_tenders():
             logging.info("Table Loaded...")
             break
         except Exception as e:
-            logging.info("Failed to Load Table...")
+            logging.info("Telebot: Failed to Load Table...")
     columns = get_tenders_columns()
     data = []
     counter = 1
@@ -130,9 +136,9 @@ def get_tenders():
             counter+=1
             time.sleep(5)
         except:
-            logging.info("No More Next Button...Exiting...!")
+            logging.info("Telebot: No More Next Button...Exiting...!")
             break
-    logging.info(f"Total Pages Scrapped: {counter}")
+    logging.info(f"Telebot: Total Pages Scrapped: {counter}")
     return columns, data
 
 def get_group_chat_id():
@@ -147,8 +153,8 @@ def get_group_chat_id():
             status = bot.get_chat_member(chat_id=id, user_id=bot.get_me().id)
             if str(status.status) != "kicked":
                 groups.append(id)
-        except:
-            pass
+        except Exception as e:
+            logging.info(f"Telebot: {e}")
     return groups
 
 def send_tenders():
@@ -170,15 +176,15 @@ def send_tenders():
                         #     allow_sending_without_reply = True,
                         #     parse_mode='HTML'
                         #     )
-                        print(msg)
+                        data_saver(msg, "data/telebot.txt")
                     time.sleep(2)
                     break
                 except ApiTelegramException as e:
                     retry+=1
                     retry_after = int(e.result_json['parameters']['retry_after'])
-                    logging.info(f"Retrying After {retry_after} seconds...")
+                    logging.info(f"Telebot: Retrying After {retry_after} seconds...")
                     time.sleep(retry_after)
-    logging.info(f"Total {int(len(tenders))+1} Tender Data sent...")
+    logging.info(f"Telebot: Total {int(len(tenders))+1} Tender Data sent...")
 
 # --------------------- Tender Scraper END -----------------------------------#
 
@@ -192,44 +198,81 @@ with open("search_keywords.txt", "r") as f:
 
 def linkedin_scraper():
     for keyword in keyword_list:
+        count = 0
         base_url = f"https://www.linkedin.com/jobs/search?keywords={keyword}&location=Denmark&geoId=104514075&f_TPR=r86400&position=1&pageNum=0"
         driver_li.get(base_url)
+        job_url= ""
         retry = 1
         max_retry = 5
         while retry<=max_retry:
             try:
-                WebDriverWait(driver_li, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, ".top-card-layout__title")))
-                job_cards = driver_li.find_elements(By.CLASS_NAME, "base-card__full-link")
+                WebDriverWait(driver_li, 10).until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, ".jobs-search__results-list")))
+                job_cards = driver_li.find_elements(By.CLASS_NAME, "base-card")
                 break
             except Exception as e:
                 retry+=1
-                print(e)
+                logging.info(f"linkedin: {e}")
         if retry >= max_retry:
-            print(f"Couldn't search for {keyword}")
+            logging.info(f"linkedin: Couldn't search for {keyword}")
             continue
-        print(len(job_cards))
+        logging.info(len(job_cards))
         for card in job_cards:
-            try:
-                WebDriverWait(driver_li, 10).until(EC.element_to_be_clickable(card))
-                card.click()
-                detail_card = driver_li.find_element(By.CLASS_NAME, "details-pane__content")
-                WebDriverWait(driver_li, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, ".top-card-layout__title")))
+            while True:
+                try:
+                    WebDriverWait(driver_li, 10).until(EC.element_to_be_clickable(card))
+                    card.click()
+
+                except Exception as e:
+                    print(f"linkedin: clicking card failed: {e}")
+                    continue
+                try:
+                    WebDriverWait(driver_li, 10).until(lambda driver_li:driver_li.find_element(By.CSS_SELECTOR, ".topcard__link"))
+                    script = f"return document.getElementsByClassName('topcard__link')[0].getAttribute('href');"
+                    check_url = driver_li.execute_script(script)
+                    print(f"--------{check_url}")
+                except:
+                    print(f"linkedin: Exce url failed: {e}")
+                    continue
+                try:
+                    WebDriverWait(driver_li, 10).until(lambda driver_li:driver_li.find_element(By.CSS_SELECTOR, ".topcard__link").get_attribute("href")==check_url)
+                    # WebDriverWait(driver_li, 10).until(lambda driver_li : driver_li.execute_script('return document.readyState')=='complete')
+                    detail_card = driver_li.find_element(By.XPATH, "/html/body/div[1]/div/section/div[2]")
+                    break
+                except Exception as e:
+                    time.sleep(2)
+                    logging.info(f"linkedin: Detail Panel was not found: {e}")
+            try:    
                 job_pos = detail_card.find_element(By.CSS_SELECTOR, ".top-card-layout__title").text
+                logging.info(f"linkedin: Job Position: {job_pos}")
+            except Exception as e:
+                logging.info(f"linkedin: Failed to fetch job position: {e}")    
+            try:    
                 location = detail_card.find_element(By.CSS_SELECTOR, "span.topcard__flavor:nth-child(2)").text
+            except Exception as e:
+                logging.info(f"linkedin: Failed to fetch location: {e}")
+            try:        
                 job_url_tag = detail_card.find_element(By.CSS_SELECTOR, ".topcard__link")
                 job_url = job_url_tag.get_attribute("href")
-                description = detail_card.find_element(By.CSS_SELECTOR, ".show-more-less-html__markup").text
-                time_ago = detail_card.find_element(By.CSS_SELECTOR, ".posted-time-ago__text").text
-                time_list = str(time_ago).split(' ')
-                # if time_list[1] == 'minutes' or time_list[1] == 'minute':    
-                print(job_pos, location)
-                print(description)
-                print(job_url)
-                print(time_ago)
-                print("=================")
-                time.sleep(5)
             except Exception as e:
-                print(e)
+                logging.info(f"linkedin: Failed to fetch job URL: {e}")
+            try:        
+                description = detail_card.find_element(By.CSS_SELECTOR, ".show-more-less-html__markup").text
+            except Exception as e:
+                logging.info(f"linkedin: Failed to fetch description: {e}")    
+                # time_ago = detail_card.find_element(By.CSS_SELECTOR, ".posted-time-ago__text").text
+                # time_list = str(time_ago).split(' ')
+                # if time_list[1] == 'minutes' or time_list[1] == 'minute':    
+                # logging.info(job_pos, location)
+                # print(description)
+                # print(job_url)
+                # print(time_ago)
+                # print("=================")
+                data_saver(job_pos+location+job_url, "data/linkedin.txt")
+                count+=1
+                time.sleep(5)
+            # except Exception as e:
+            #     logging.info(f"linkedin: Job position: {job_pos} \n {e}")
+        data_saver(f"-----------{count}---------", "data/linkedin.txt")
 #----------------------Jobindex Scraper--------------------------------#
 def ignoring_popups(url, driver):
     driver.get(url)
@@ -237,12 +280,12 @@ def ignoring_popups(url, driver):
     try:
         btn = driver.find_element(By.XPATH, '//*[@id="jix-cookie-consent-accept-all"]')
         btn.click()
-        print("Cookies Accepted...")
+        logging.info("Cookies Accepted...")
         cross_btn = driver.find_element(By.XPATH, '//*[@id="jobmail_popup"]/div/div/div/button/span')
         cross_btn.click()
         time.sleep(2)
     except Exception as e:
-        print(e)
+        logging.info(f"Pop up: {e}")
 
 def jobindex_scraper():
     ignoring_popups("https://www.jobindex.dk/jobsoegning/danmark?jobage=1&lang=en", driver_ji)
@@ -253,7 +296,7 @@ def jobindex_scraper():
         while True:
             WebDriverWait(driver_ji, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "jobsearch-result")))
             results = driver_ji.find_elements(By.CLASS_NAME, "jobsearch-result")
-            print(len(results))
+            logging.info(len(results))
             for result in results:
                 WebDriverWait(driver_ji, 10).until(EC.presence_of_all_elements_located((By.CLASS_NAME, "jix_robotjob--area")))
                 job_pos = result.find_element(By.TAG_NAME, "h4").text               
@@ -268,11 +311,12 @@ def jobindex_scraper():
                 description = ""
                 for d in descriptions:
                     description = description+d.text
-                print(f"Position: {job_pos}")
-                print(f"Locations: {location}")
-                print(f"Job URL: {job_url}")
-                print(f"JD: {description}")
-                print("===================")
+                # logging.info(f"Position: {job_pos}")
+                # logging.info(f"Locations: {location}")
+                # logging.info(f"Job URL: {job_url}")
+                # logging.info(f"JD: {description}")
+                # logging.info("===================")
+                data_saver(job_pos+location+job_url+description, "data/jobindex.txt")
                 time.sleep(2)
             try:
                 pag = driver_ji.find_element(By.CLASS_NAME, "jix_pagination")
@@ -282,7 +326,7 @@ def jobindex_scraper():
                 driver_ji.get(url)
                 time.sleep(2)
             except:
-                print("No more pages...")
+                logging.info("jobindex: No more pages...")
                 time.sleep(2)
                 break
 
@@ -298,7 +342,7 @@ def jobbank_scrapper():
         while True:
             WebDriverWait(driver_jb, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "results")))
             results = driver_jb.find_elements(By.CLASS_NAME, "jobsearch-result")
-            print(len(results))
+            logging.info(len(results))
             for result in results:
                 WebDriverWait(driver_jb, 10).until(EC.presence_of_all_elements_located((By.CLASS_NAME, "job-location")))
                 job_head = result.find_element(By.TAG_NAME, "h3")
@@ -314,11 +358,12 @@ def jobbank_scrapper():
                 description = ""
                 for d in descriptions:
                     description = description+d.text
-                print(f"Position: {job_pos}")
-                print(f"Locations: {location}")
-                print(f"Job URL: {job_url}")
-                print(f"JD: {description}")
-                print("===================")
+                # logging.info(f"Position: {job_pos}")
+                # logging.info(f"Locations: {location}")
+                # logging.info(f"Job URL: {job_url}")
+                # logging.info(f"JD: {description}")
+                # logging.info("===================")
+                data_saver(job_pos+location+job_url+description, "data/jobbank.txt")
                 time.sleep(2)
             try:
                 pag = driver_jb.find_element(By.CLASS_NAME, "jix_pagination")
@@ -328,10 +373,35 @@ def jobbank_scrapper():
                 driver_jb.get(url)
                 time.sleep(2)
             except:
-                print("No more pages...")
+                logging.info(f"jobbank: No more pages...")
                 time.sleep(2)
                 break
 
+
+#---------------------indeed scraper----------------------#
+
+# def indeed_scraper():
+#     for keyword in keyword_list:
+#         keyword = keyword.replace(" ", "+")
+#         base_url = f"https://www.indeed.com/jobs?q={keyword}&l=Denmark&fromage=1&vjk=fe184b987b836188"
+#         driver_in.get(base_url)
+#         time.sleep(20)
+        # retry = 1
+        # max_retry = 5
+        # while retry<=max_retry:
+        #     try:
+        #         WebDriverWait(driver_in, 10).until(EC.visibility_of_element_located((By.CLASS_NAME, "jcs-JobTitle")))
+        #         job_cards = driver_in.find_elements(By.CLASS_NAME, "resultContent")
+        #         break
+        #     except Exception as e:
+        #         retry+=1
+        #         print(e)
+        # if retry >= max_retry:
+        #     print(f"Couldn't search for {keyword}")
+        #     continue
+
+        # print(len(job_cards))
+        
 
 
 # --------------------- Job Scraper End -----------------------------------#
@@ -344,13 +414,15 @@ def job_listener(event):
 
 # if __name__ == "__main__":
 #     scheduler = BlockingScheduler()
-#     scheduler.add_job(send_tenders, 'interval', minutes = 5)
-#     scheduler.add_job(linkedin_scraper, 'interval', minutes = 5)
+#     scheduler.add_job(send_tenders, 'interval', minutes = 60)
+#     scheduler.add_job(linkedin_scraper, 'interval', minutes = 60)
+#     scheduler.add_job(jobindex_scraper, 'interval', minutes = 60)
+#     scheduler.add_job(jobbank_scrapper, 'interval', minutes = 60)
 #     scheduler.start()
 #     scheduler.add_listener(job_listener)
 #     driver.quit()
 #     driver_li.quit()
 
 if __name__ == "__main__":
-    jobbank_scrapper()
+    linkedin_scraper()
     driver_li.quit()
